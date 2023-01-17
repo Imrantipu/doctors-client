96c01942266427f6a4030a19fbef083fef5bc4f5
Imran <imrantipu90@yahoo.com>
2023-01-17 18:12:45 +0100

    info section crated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,9 +6,9 @@ const Banner = () => {
     <div>
       <div className="hero rounded-xl" style={{backgroundImage: `url(${bgchair})`}}>
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src={chair} className="rounded-lg shadow-2xl w-1/2" alt=""/>
+    <img src={chair} className="rounded-lg shadow-2xl md:w-1/2 sm:w-full" alt=""/>
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <h1 className="text-5xl font-bold">YOUR NEW SMILE STARTS HERE</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
