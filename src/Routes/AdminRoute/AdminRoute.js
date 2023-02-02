30b85104925b02146c0c2c2a4b0f4504375cf6b0
Imran <imrantipu90@yahoo.com>
2023-02-02 20:02:08 +0100

    admin route add loader to fetch data

--- src/Routes/AdminRoute/AdminRoute.js
+++ src/Routes/AdminRoute/AdminRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext} from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import Loader from '../../components/Loader/Loader';
 import { AuthContext } from '../../contexts/AuthProvider';
@@ -8,10 +8,10 @@ import useAdmin from './../../hooks/useAdmin';
 const AdminRoute = ({ children }) => {
 
     const {user,loading} = useContext(AuthContext);
-    const [isAdmin] = useAdmin(user?.email);
+    const [isAdmin,isAdminLoading] = useAdmin(user?.email);
     let location = useLocation();
 
-    if(loading){
+    if(loading || isAdminLoading){
         return <Loader></Loader>
     }
 
