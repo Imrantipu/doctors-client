51e8d50dc6511e0942ad465a318225f16236cd6d
Imran <imrantipu90@yahoo.com>
2023-02-04 11:29:25 +0100

    Loder used when loader used in router

--- src/Payment/Payment.js
+++ src/Payment/Payment.js
@@ -1,13 +1,20 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
+import Loader from "../components/Loader/Loader";
 import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 const Payment = () => {
   const booking = useLoaderData();
+//   when using loader in route useNavigation helps to identify loading state
+// to use Loader
+  const navigation = useNavigation();
   const { treatment, price, appointmentDate, slot } = booking;
+   if(navigation.state === "loading"){
+        return <Loader></Loader>
+    }
   return (
     <div>
       <h3 className="text-3xl">Payment for {treatment}</h3>
